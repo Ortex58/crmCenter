9aceda0d35b01062811cfc03abbf8e770e0d4cfa
Breez <ortex@i.ua>
2018-04-17 21:37:06 +0300

    laptop 2

--- js/index.js
+++ js/index.js
@@ -7,3 +7,10 @@ $('.header__content__slider').slick({
 $(".sandwich, .menu_item").click(function() {
   $(".sandwich").toggleClass("active");
 });
+
+//show menu click
+		$(".header__menu__btn").click(function(){
+		    $(".mobile__nav").slideToggle(1500);
+		    $(".header__content__slider__slide__content").slideToggle(1500);
+		   
+		});
